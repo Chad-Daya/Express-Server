6250729b989ce548c138cff17c080fe86e448db5
Chad-Daya <zeac144@live.rhul.ac.uk>
2020-11-05 11:23:26 +0000

    Express Server
    Testing First Server Set up

---
+++ server.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();	
+
+app.get("/", function(request, response){ 
+	response.send("Hello World!");
+});	
+
+
+app.listen(3000, function(){
+	console.log("server started on port 3000");
+});
